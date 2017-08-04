f2b3bf1c7099295c6a7514b861aceb3b3468bf60
Sebastian Haas <sehaas@deebas.com>
2017-08-04 23:29:21 +0200

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpackConfig = {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
 	entry: {
 		app: './src/index.js'
 	},
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
-    }
+	},
+	devServer: {
+		contentBase: './dist'
+	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
