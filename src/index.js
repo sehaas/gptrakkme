7b5e443e5fc2d85c0e1b60b48c6b897e5145ea84
Sebastian Haas <sehaas@deebas.com>
2017-08-20 01:03:47 +0200

    add coordist for distance calculation

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ require('leaflet-fullscreen');
 var gpx2geojson = require('@mapbox/togeojson').gpx;
 var d3 = require('d3');
 
+var coordist = require('coordist');
+
 const gptrakkme = window.gptrakkme = {};
 
 // create map
@@ -46,18 +48,45 @@ gptrakkme.renderLayer = function(str) {
 	var geojson = gpx2geojson(dom);
 
 	var props = geojson.features[0].properties,
-		coords = geojson.features[0].geometry.coordinates,
-		datePlaceHeart = props.coordTimes.map(function(d, i) {
+		coords = geojson.features[0].geometry.coordinates;
+
+	var dist = 0.0,
+		hM_a = 0.0,
+		hM_d = 0.0,
+		meter = d3.format(">7,d"),
+		last = {lng:coords[0][0], lat:coords[0][1], alt:coords[0][2]};
+
+	props.distance = [0.0];
+	props.speed = [0.0];
+	for(var i = 1; i < coords.length; i++) {
+		var curr = {lng:coords[i][0], lat:coords[i][1], alt:coords[i][2]};
+		var hm = curr.alt - last.alt;
+		if (hm > 0) {
+			hM_a += hm;
+		} else {
+			hM_d += hm;
+		}
+		var d = coordist.distance(last, curr, false);
+		var dt = new Date(props.coordTimes[i]) - new Date(props.coordTimes[i-1]);
+		props.speed[i] = 3600 * d / dt;
+		props.distance[i] = d;
+		dist += d;
+		last = curr;
+	}
+
+
+	var datePlaceHeart = props.coordTimes.map(function(d, i) {
 			var hr = 0; // support tracks without HR data
 			if (props.heartRates) {
 				hr=props.heartRates[i];
 			}
-			return [new Date(d), coords[i], hr];
+			return [new Date(d), coords[i], hr, props.speed[i]];
 		}),
 		bisectPlace = d3.bisector(function(d) { return d[0]; }).left,
 		height = 100,
-		width = 600,
-		margin = 20;
+		margin = 20,
+		width = d3.select('body').node().offsetWidth - 2 * margin;
+
 
 	// render map layers
 	var casingLayer = L.geoJson(geojson, {
@@ -72,11 +101,7 @@ gptrakkme.renderLayer = function(str) {
 	hereMarker.bringToFront();
 
 	var x = d3.scaleTime()
-		.domain([d3.min(datePlaceHeart, function(d) {
-			return d[0];
-		}), d3.max(datePlaceHeart, function(d) {
-			return d[0];
-		})])
+		.domain([datePlaceHeart[0][0], datePlaceHeart[datePlaceHeart.length-1][0]])
 		.rangeRound([0, width]);
 	var elevation = d3.scaleLinear()
 		.range([height, 0])
@@ -84,18 +109,34 @@ gptrakkme.renderLayer = function(str) {
 			return d[1][2];
 		})]);
 
+	var maxHeart = d3.max(datePlaceHeart, function(d) { return d[2]; });
+	var avgHeart = d3.mean(datePlaceHeart, function(d) { return d[2]; });
+	var heart = d3.scaleLinear()
+		.range([height, 0])
+		.domain([0, maxHeart]);
+
+	var maxSpeed = d3.max(datePlaceHeart, function(d) { return d[3]; });
+	var avgSpeed = d3.mean(datePlaceHeart, function(d) { return d[3]; });
+	var speed = d3.scaleLinear()
+		.range([height, 0])
+		.domain([0, d3.max(datePlaceHeart, function(d) {
+			return d[3];
+		})]);
+
 	var elevationLine = d3.area()
 		.x(function(d) { return x(d[0]); })
 		.y0(height)
 		.y1(function(d) { return elevation(d[1][2]); });
 
-	var moveMarker = function(evt){
-		var posX = d3.mouse(this)[0];
-		d3.selectAll('.here-indicator').attr('transform', 'translate(0,0)');
-		marker.attr('transform', 'translate(' + [posX, 0] + ')');
-		var datum = datePlaceHeart[bisectPlace(datePlaceHeart, x.invert(posX))];
-		hereMarker.setLatLng(L.latLng(datum[1][1], datum[1][0]));
-	};
+	var speedLine = d3.line()
+		.curve(d3.curveBasis)
+		.x(function(d) { return x(d[0]); })
+		.y(function(d) { return speed(d[3]); });
+
+	var heartLine = d3.line()
+		.curve(d3.curveBasis)
+		.x(function(d) { return x(d[0]); })
+		.y(function(d) { return heart(d[2]); });
 
 	var svg = d3.select('body').append('svg')
 		.attr('width', width + 2 * margin)
@@ -107,11 +148,44 @@ gptrakkme.renderLayer = function(str) {
 		.datum(datePlaceHeart)
 		.attr('class', 'elevation-area')
 		.attr('d', elevationLine);
+	g.append('path')
+		.datum(datePlaceHeart)
+		.attr('class', 'heart-line')
+		.attr('d', heartLine);
+	g.append('path')
+		.datum(datePlaceHeart)
+		.attr('class', 'speed-line')
+		.attr('d', speedLine);
 
 	var marker = g.append('rect')
 		.attr('width', 1)
 		.attr('class', 'here-indicator')
 		.attr('height', height);
+
+
+	var statusDiv = d3.select('body').append('div').attr('class', 'status-box');
+	var markerDiv = statusDiv.append('div').attr('class', 'marker-box');
+	var heightText = markerDiv.append('div').attr('class', 'marker-value');
+	var heartText = markerDiv.append('div').attr('class', 'marker-value');
+	var speedText = markerDiv.append('div').attr('class', 'marker-value');
+
+	var totalDiv = statusDiv.append('div').attr('class', 'total-box');
+	var zeit = datePlaceHeart[datePlaceHeart.length-1][0] - datePlaceHeart[0][0];
+	totalDiv.append('div').text('Distanz: ' + meter(dist) + 'm / Zeit: ' + d3.utcFormat("%H:%M")(zeit));
+	totalDiv.append('div').text('Geschwindigkeit: max ' + meter(maxSpeed) + 'km/h / avg ' + meter(avgSpeed) + 'km/h');
+	totalDiv.append('div').text('Höhenmeter: ' + meter(hM_a) + 'm / ' + meter(hM_d) + 'm');
+	totalDiv.append('div').text('Puls: max ' + meter(maxHeart) + 'bpm / avg ' + meter(avgHeart) + 'bpm');
+
+	var moveMarker = function(evt){
+		var posX = d3.mouse(this)[0];
+		d3.selectAll('.here-indicator').attr('transform', 'translate(0,0)');
+		marker.attr('transform', 'translate(' + [posX, 0] + ')');
+		var datum = datePlaceHeart[bisectPlace(datePlaceHeart, x.invert(posX))];
+		hereMarker.setLatLng(L.latLng(datum[1][1], datum[1][0]));
+		heightText.text(d3.format("0.2f")(datum[1][2]) + "m");
+		heartText.text(datum[2] + "bpm");
+		speedText.text(d3.format("0.2f")(datum[3]) + "km/h");
+	};
 
 	svg.append("rect")
 		.attr("fill", "transparent")
